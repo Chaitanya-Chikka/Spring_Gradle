2dc0a0e5c44731b42cef8d5e873d378ca1cc56d6
Chaitanya-Chikka <158842901+Chaitanya-Chikka@users.noreply.github.com>
2024-02-04 22:08:12 -0500

    Add files via upload

---
+++ src/main/java/com/apiproject/api_nasa/util/GlobalConstants.java
@@ -0,0 +1,10 @@
+
+
+package com.apiproject.api_nasa.util;
+
+public class GlobalConstants {
+	
+	public static final String APOD_URL = "https://api.nasa.gov/planetary/apod?api_key=zsoCHuhYM0po6lybSy37xjm9OrSzlFZrTJcwl0GY";
+	
+	
+}
